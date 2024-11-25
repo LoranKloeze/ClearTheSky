0b93d7b45e188861833fc1bdbb1c7f4be81e05c4
Loran Kloeze <loran@codedivision.nl>
2024-11-25 10:16:39 +0100

    Add AuthService for JWT handling
    Introduced an AuthService that generates JWT access and refresh tokens. AuthService uses claims based on user data and signs tokens with a private key from configuration. Added corresponding IAuthService interface to define contract methods.

---
+++ Backend.Web/Services/AuthService.cs
@@ -0,0 +1,56 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Backend.Web.Models;
+using Backend.Web.Services.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Backend.Web.Services;
+
+public class AuthService(IConfiguration configuration) : IAuthService
+{
+    public string GenerateAccessToken(User user)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(PrivateKey);
+        var credentials = new SigningCredentials(
+            new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha256Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = GenerateClaims(user),
+            Expires = DateTime.UtcNow.AddMinutes(15),
+            SigningCredentials = credentials
+        };
+
+        var token = handler.CreateToken(tokenDescriptor);
+        return handler.WriteToken(token);
+    }
+    
+    public string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+    
+    private static ClaimsIdentity GenerateClaims(User user)
+    {
+        var claims = new ClaimsIdentity();
+        claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.BlueskyDid));
+        return claims;
+    }
+
+    private string PrivateKey
+    {
+        get
+        {
+            var authOptions = new AuthOptions();
+            configuration.GetSection(AuthOptions.Auth).Bind(authOptions);
+            return authOptions.PrivateKey;
+        }
+    }
+}
---
+++ Backend.Web/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using Backend.Web.Models;
+
+namespace Backend.Web.Services.Interfaces;
+
+public interface IAuthService
+{
+    string GenerateAccessToken(User user);
+    string GenerateRefreshToken();
+}
