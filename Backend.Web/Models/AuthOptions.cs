2183ff96ffbd84d7b7a0bfcac34fa74593b99ec0
Loran Kloeze <loran@codedivision.nl>
2024-11-25 10:17:08 +0100

    Add authentication and authorization functionalities
    Introduced JWT-based authentication and authorization using `AuthOptions`. Configured `Swagger` for secure endpoints and removed placeholder weather forecast endpoints.

---
+++ Backend.Web/Models/AuthOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Web.Models;
+
+public class AuthOptions
+{
+    public const string Auth = "Auth";
+
+    // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
+    public string PrivateKey { get; set; } = string.Empty;
+}
--- Backend.Web/Program.cs
+++ Backend.Web/Program.cs
@@ -1,18 +1,112 @@
+using System.Text;
+using Backend.Web.Data;
+using Backend.Web.Models;
+using Backend.Web.Services;
+using Backend.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Npgsql;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
+    .MinimumLevel.Debug()
     .WriteTo.Console()
     .CreateLogger();
 
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+dataSourceBuilder.EnableDynamicJson();
+var dataSource = dataSourceBuilder.Build();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(dataSource);
+});
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter your JWT token in this field",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    };
+    c.AddSecurityDefinition("Bearer", securityScheme);
+    var securityRequirement = new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            []
+        }
+    };
+
+    c.AddSecurityRequirement(securityRequirement);
+    
+});
 builder.Services.AddSerilog();
+builder.Services.AddControllers();
+
+var configuration = builder.Configuration;
+var authOptions = new AuthOptions();
+configuration.GetSection(AuthOptions.Auth).Bind(authOptions);
+if (string.IsNullOrWhiteSpace(authOptions.PrivateKey))
+{
+    throw new Exception("Private key is not set, set it in configuration key Auth:PrivateKey");
+}
+builder.Services
+    .AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(authOptions.PrivateKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+builder.Services.AddAuthorization();
+
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IFishyFlipService, FishyFlipService>();
+builder.Services.AddSingleton<IEncryptionService, EncryptionService>(provider =>
+{
+    var config = provider.GetRequiredService<IConfiguration>();
+    var encryptionKey = config["EncryptionSettings:Key"];
+    if (string.IsNullOrWhiteSpace(encryptionKey))
+    {
+        throw new Exception("Encryption key is not set, set it in configuration key EncryptionSettings:Key");
+    }
+    return new EncryptionService(encryptionKey);
+});
+
 
 var app = builder.Build();
-
+app.UseAuthentication();
+app.UseAuthorization();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,30 +114,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
+app.MapControllers();
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
