bdedba0be2df7a4221590e302ca49d0397db2480
Loran Kloeze <loran@codedivision.nl>
2024-11-27 21:29:04 +0100

    Setup landing/login page

---
+++ Frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+﻿
+// Import svg from assets/logo.svg
+import logo from '../assets/logo.svg'
+
+const LoginPage = () => {
+    return (
+        <div className="flex justify-center items-center h-screen w-screen">
+            <img className="w-1/6" src={logo} alt="Logo ClearTheSky"/>
+        </div>
+    )
+
+}
+
+export default LoginPage
---
+++ Frontend/src/routes.tsx
@@ -0,0 +1,11 @@
+﻿import {createBrowserRouter} from "react-router"
+import LoginPage from "./pages/LoginPage.tsx"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LoginPage />,
+    },
+    
+])
+export default router
